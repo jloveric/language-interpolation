1e338e7b0e3480cf86a880208098bfd53c3713bf
John <john.loverich@gmail.com>
2022-06-05 20:43:13 -0700

    Adding additional tests

--- tests/test_datsets.py
+++ tests/test_datsets.py
@@ -1,15 +1,36 @@
 import pytest
-from language_interpolation.single_text_dataset import dataset_from_gutenberg
+from language_interpolation.single_text_dataset import (
+    dataset_from_gutenberg,
+    dataset_from_file,
+)
 
 
 def test_dataset_from_gutenberg():
-    features = 10
-    targets = 1
+    num_features = 10
+    num_targets = 1
 
     features, targets = dataset_from_gutenberg(
-        gutenberg_id=1000, features=features, targets=targets, max_size=-1
+        gutenberg_id=1000, features=num_features, targets=num_targets, max_size=-1
     )
 
     assert features.shape[0] == targets.shape[0]
-    assert features.shape[1] == features
-    assert targets.shape[1] == targets
+    assert features.shape[1] == num_features
+    assert targets.shape[1] == num_targets
+
+
+def test_dataset_from_file():
+    num_features = 10
+    num_targets = 1
+
+    features, targets = dataset_from_file(
+        filename="books/frankenstein.txt",
+        features=num_features,
+        targets=num_targets,
+        max_size=-1,
+    )
+
+    print("features.shape", features.shape, targets.shape)
+
+    assert features.shape[0] == targets.shape[0]
+    assert features.shape[1] == num_features
+    assert targets.shape[1] == num_targets
