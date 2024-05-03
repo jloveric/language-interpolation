d02d866332a200ee6cb0cc170b2d76cc7a5c1266
jloveric <john.loverich@gmail.com>
2024-05-02 20:23:24 -0700

    Stubs for a new network

---
+++ language_interpolation/dual_convolutional_network.py
@@ -0,0 +1,12 @@
+"""
+Convolutional network that also shares
+depth wise convolutions
+"""
+import torch
+
+class DualConvolutionalNetwork(torch.nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def other():
+        pass
